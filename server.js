93fce62f77d8d48b70e3677e1a1ed9b7b79316d5
achrafkeddour <152210432+achrafkeddour@users.noreply.github.com>
2024-03-22 02:43:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-// server.js
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const { Pool } = require('pg');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+
+const pool = new Pool({
+    host: 'dpg-cnu9h5a0si5c73ds58mg-a.oregon-postgres.render.com',
+    user: 'keddour',
+    password: 'ajHdWafoMvmbFB8erfBgqkdjafOUxdzU',
+    database: 'achraf_16qa',
+    port: 5432, // PostgreSQL port
+    ssl: {
+        rejectUnauthorized: false // For Render's PostgreSQL SSL
+    }
+});
+
 
 // Serve static files from the current directory
 app.use(express.static(__dirname));
@@ -30,18 +41,25 @@ io.on('connection', (socket) => {
         }
     });
 
-    
     // Handle private messages
-socket.on('private message', ({ recipient, message }) => {
-    const recipientSocketId = users[recipient];
-    if (recipientSocketId) {
-        io.to(recipientSocketId).emit('open conversation', socket.username);
-        io.to(recipientSocketId).emit('private message', { sender: socket.username, message }); // Include sender's username
-    } else {
-        socket.emit('errorMessage', `User ${recipient} is not connected.`);
-    }
-});
+    socket.on('private message', async ({ recipient, message }) => {
+        const recipientSocketId = users[recipient];
+        if (recipientSocketId) {
+            io.to(recipientSocketId).emit('open conversation', socket.username);
+            io.to(recipientSocketId).emit('private message', { sender: socket.username, message }); // Include sender's username
 
+            try {
+                // Store the message in the database
+                const client = await pool.connect();
+                await client.query('INSERT INTO messages (sender, receiver, message) VALUES ($1, $2, $3)', [socket.username, recipient, message]);
+                client.release();
+            } catch (error) {
+                console.error('Error storing message in the database:', error);
+            }
+        } else {
+            socket.emit('errorMessage', `User ${recipient} is not connected.`);
+        }
+    });
 
     // Handle disconnection
     socket.on('disconnect', () => {
